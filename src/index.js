fdf062aeb5c46ada22314a4b16c33b6c40e90ff8
GR22 <bagbox@mail.ru>
2021-07-14 12:13:11 +0400

    add morse decoder task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let result = '';
+      for (let index = 0; index < expr.length; index+=10) {
+        let binSimbol = expr.slice(index, index+10)
+        if (binSimbol=='**********') {
+          result+=' ';
+          continue;
+        }
+        console.log(binSimbol);
+        let morseSimbol='';
+        for (let j = 0; j < binSimbol.length; j+=2) {      
+          let morseBit = binSimbol.slice(j, j+2);
+          if (morseBit == 00) continue;
+          (morseBit == 10) ? morseSimbol+='.': morseSimbol+='-';      
+          }
+        for (const key in MORSE_TABLE) {
+          if (key==morseSimbol) {
+            result+=MORSE_TABLE[key];
+            console.log(MORSE_TABLE[key])
+            continue;
+          }
+        }    
+      }
+      return result;
+    }
 
 module.exports = {
     decode
